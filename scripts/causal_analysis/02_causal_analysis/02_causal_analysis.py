eae0aa4aec6dcecedefb2a02567b93984a404d5c
Alejandro Velasco <svelascodimate@wm.edu>
2025-06-05 15:37:46 +0000

    Modified script causal analisis

--- scripts/causal_analysis/02_causal_analysis/02_causal_analysis.py
+++ scripts/causal_analysis/02_causal_analysis/02_causal_analysis.py
@@ -133,44 +133,60 @@ def compute_causal_effects(causal_model):
 
 
 # %%
-### Compute causal effect for treatment
-def compute_causal_effect_hypothesis(causal_hypothesis_df, outcomes):
+def compute_causal_effect_for_treatments(causal_hypothesis_df, outcomes):
+    """
+    Computes causal effects for each treatment (excluding control) in the given DataFrame.
+    Returns a list of result DataFrames, one per treatment.
+    """
     causal_effect_dfs = []
     for treatment in (t for t in causal_hypothesis_df['treatment'].unique() if t != params['intervention']['control']):
-
         print(f"=========== CAUSAL ANALYSIS FOR TREATMENT-{treatment}")
-        #### Extract treatment and control dataframes
-        treatment_df = causal_hypothesis_df[causal_hypothesis_df['treatment']==treatment].copy()
-        control_df = causal_hypothesis_df[causal_hypothesis_df['treatment']==params['intervention']['control']].copy()
+        # Extract treatment and control dataframes
+        treatment_df = causal_hypothesis_df[causal_hypothesis_df['treatment'] == treatment].copy()
+        control_df = causal_hypothesis_df[causal_hypothesis_df['treatment'] == params['intervention']['control']].copy()
 
-        ### Ensure treatment_df and control_df have the same size, choose the smallest size
+        # Ensure treatment_df and control_df have the same size
         min_size = min(len(treatment_df), len(control_df))
-        #min_size = 1000 ### ONLY FOR TESTING
         treatment_df = treatment_df.sample(n=min_size, random_state=42).reset_index(drop=True)
         control_df = control_df.sample(n=min_size, random_state=42).reset_index(drop=True)
 
-        ### Combine treatment and control
+        # Combine treatment and control
         intervention_df = pd.concat([treatment_df, control_df], ignore_index=True)
 
-        ### Set binnary_treatment column
-        intervention_df['binary_treatment'] = intervention_df['treatment'].map(lambda group: True if group == treatment else False)
+        # Set binary_treatment column
+        intervention_df['binary_treatment'] = intervention_df['treatment'].map(lambda group: group == treatment)
 
-        ##### Define causal model 
+        # Define causal model 
         causal_model = CausalModel(
-            data = intervention_df,
-            treatment = ['binary_treatment'],
-            outcome = outcomes,
-            common_causes = params['causal_analysis']['common_causes'](params['features']['syntactic'] + params['features']['semantic']),
-            effect_modifiers = params['causal_analysis']['effect_modifiers'](None),
-            instruments = params['causal_analysis']['instruments'](None))
-        
-        ##### Store results
+            data=intervention_df,
+            treatment=['binary_treatment'],
+            outcome=outcomes,
+            common_causes=params['causal_analysis']['common_causes'](params['features']['syntactic'] + params['features']['semantic']),
+            effect_modifiers=params['causal_analysis']['effect_modifiers'](None),
+            instruments=params['causal_analysis']['instruments'](None)
+        )
+
+        # Store results
         result_df = compute_causal_effects(causal_model)
         result_df['intervention'] = treatment
         causal_effect_dfs.append(result_df)
+    return causal_effect_dfs
+
+def compute_causal_effects_smells(causal_hypothesis_df, outcomes):
+    s_msg_id_effects = {}
+    for s_msg_id in causal_hypothesis_df['s_msg_id'].unique():
+        print(f"=========== CAUSAL ANALYSIS FOR CODE SMELL-{s_msg_id} ===========")
+        # Subset for current s_msg_id
+        causal_hypothesis_subset = causal_hypothesis_df[causal_hypothesis_df['s_msg_id'] == s_msg_id].copy()
+        causal_effect_dfs = compute_causal_effect_for_treatments(causal_hypothesis_subset, outcomes)
+        s_msg_id_effects_df = pd.concat(causal_effect_dfs, ignore_index=True)
+        s_msg_id_effects_df['s_msg_id'] = s_msg_id
+        s_msg_id_effects[s_msg_id] = s_msg_id_effects_df
+    return s_msg_id_effects
+
+def compute_causal_effect_hypothesis(causal_hypothesis_df, outcomes):
+    causal_effect_dfs = compute_causal_effect_for_treatments(causal_hypothesis_df, outcomes)
     return pd.concat(causal_effect_dfs, ignore_index=True)
-
-
 
 # %%
 def create_folder(path):
@@ -181,6 +197,17 @@ def create_folder(path):
 # ### EXECUTE
 
 # %%
+def execute_analysis_per_smell(outcomes, causal_hypothesis_df):
+    for outcome in outcomes:
+        print(f"============================= COMPUTING ANALYSIS FOR OUTCOME-{outcome} ==================================== ")
+        causal_effect_outcome_dict = compute_causal_effects_smells(causal_hypothesis_df, [outcome])
+        print(f"============================= STORING ANALYSIS FOR OUTCOME-{outcome} ==================================== ")
+        output_path = f"{params['output_path']}/{params['intervention']['type']}"
+        create_folder(output_path)
+        causal_effects_outcome_df = pd.concat(causal_effect_outcome_dict.values(), ignore_index=True)
+        causal_effects_outcome_df.to_json(f"{output_path}/{[outcome]}.json")
+
+# %%
 def execute_analysis(outcomes, causal_hypothesis_df):
     for outcome in outcomes:
         print(f"============================= COMPUTING ANALYSIS FOR OUTCOME-{outcome} ==================================== ")
@@ -188,12 +215,12 @@ def execute_analysis(outcomes, causal_hypothesis_df):
         print(f"============================= STORING ANALYSIS FOR OUTCOME-{outcome} ==================================== ")
         output_path = f"{params['output_path']}/{params['intervention']['type']}"
         create_folder(output_path)
-        causal_effects_outcome_df.to_json(f"{output_path}/{[outcome]}.json")
+        causal_effects_outcome_df.to_json(f"{output_path}/{[outcome]}_ALL.json")
 
 # %%
-list(causal_hypothesis_df['treatment'].unique())
-
-# %%
+print("########################### Executing causal analysis per smell ###########################")
+execute_analysis_per_smell(params['causal_analysis']['potential_outcomes'], causal_hypothesis_df)
+print("########################### Executing causal analysis ALL smells ###########################")
 execute_analysis(params['causal_analysis']['potential_outcomes'], causal_hypothesis_df)
 
 
